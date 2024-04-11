9ba48af4c7326bcec8e8ce70057e8b29dab13672
Bryan Apellanes <bryan@threeheadz.com>
2024-04-11 07:20:37 -0500

    wip

--- bam.data.schema/Schema/ReverseDaoSchemaExtractor.cs
+++ bam.data.schema/Schema/ReverseDaoSchemaExtractor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Bam.Data;
 using Bam.Net.Data.Repositories;
 using Bam.Net.ServiceProxy.Encryption;
 using CsQuery.ExtensionMethods;
